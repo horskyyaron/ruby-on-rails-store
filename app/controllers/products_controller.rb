71c70ff57ce6868c7d5df30beb7fe2f42936c2b3
Yaron Horsky <horsky.yaron@gmail.com>
2025-08-13 18:30:57 +0300

    added new action, can now create productss

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,4 +13,46 @@ class ProductsController < ApplicationController
     # then we use it to query the db to find it
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  # handles the data submitted by the form
+  # but it needs to be filtered for security, thats where product_params comes in
+  # it tells rails to inspect the params and ensure there is a key names :product
+  # with an array of paramaters, the only one permitted is :name, it will 
+  # ignore any other paramater
+  #
+  def create
+    @product = Product.new(product_params)
+    # this will try and save to db, it will run validation
+    # if successfull, we want to redirect to the new product.
+    # when redirect_to is given an Active Record object, rails generate the a path
+    # for that record's *show* action
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
